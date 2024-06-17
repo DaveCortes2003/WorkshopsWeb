ffde288b53d61ab4314e40b73b9c376c4fc3b5d6
DaveCortes2003 <deivyduo@gmail.com>
2024-06-17 01:14:42 -0600

    Workshop_4 done

--- Workshop_4/server/controllers/majorController.js
+++ Workshop_4/server/controllers/majorController.js
@@ -51,14 +51,15 @@ const majorGet = (req, res) => {
         if (req.query && req.query.name) {
             let order = null;
             if (req.query.sort === "asc") {
-                order = {name : 1};
+                order = { name: 1 };
             }
             else if (req.query.sort === "desc") {
-                order = {name :-1};
+                order = { name: -1 };
             }
 
             Major.find({ "name": { "$regex": req.query.name, "$options": "i" } }).sort(order)
                 .then(majors => {
+                    res.status(200);
                     res.json(majors);
                 })
                 .catch(err => {
@@ -66,6 +67,19 @@ const majorGet = (req, res) => {
                     res.json({ "error": err });
                 });
         }
+
+        else {
+            Major.find()
+                .then(majors => {
+                    res.status(200);
+                    res.json(majors);
+                })
+                .catch(err => {
+                    res.status(422);
+                    res.json({ "error": err });
+                });
+        }
+
         /*
         Major.find()
             .then(majors => {
